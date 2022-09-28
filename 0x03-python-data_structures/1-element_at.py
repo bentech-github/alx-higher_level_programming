c47807e4331e5bf3cc949d7d37464e15c9728849
Ben <bensonadedayo123@gmail.com>
2022-09-28 01:33:23 +0000

    okayed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         response = None
-    elif idx > len(my_list):
+    elif idx > (len(my_list) - 1):
         response = None
     else:
         response = my_list[idx]
